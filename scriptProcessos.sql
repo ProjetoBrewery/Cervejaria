803b4b615896891181f6efa6ef380f5fd72f6dd6
GabrielYukioMC <126337393+GabrielYukioMC@users.noreply.github.com>
2023-05-02 15:55:04 -0300

    sql

--- scriptProcessos.sql
+++ scriptProcessos.sql
@@ -1,27 +1,27 @@
 use cervejaria;
 show tables;
-desc processos;
+desc processos3;
 
- create table processos (
+ create table processos3 (
 id int primary key auto_increment,
-maceracao1 decimal(4,2),
-malteacao1 decimal(4,2),
-malteacao2 decimal(4,2),
-malteacao3 decimal(4,2),
-moagem1 decimal(4,2),
-brassagem1 decimal(4,2),
-brassagem2 decimal(4,2),
-brassagem3 decimal(4,2),
-fervura1 decimal(4,2),
-resfriamento1 decimal(4,2),
-resfriamento2 decimal(4,2),
-resfriamento3 decimal(4,2),
-fermentacao1 decimal(4,2),
-maturacao1 decimal(4,2),
-pasteurizacao1 decimal(4,2),
-servir1 decimal(4,2)
+maceracao decimal(5,2),
+malteacao1 decimal(5,2),
+malteacao2 decimal(5,2),
+malteacao3 decimal(5,2),
+moagem decimal(5,2),
+brassagem1 decimal(5,2),
+brassagem2 decimal(5,2),
+brassagem3 decimal(5,2),
+fervura decimal(5,2),
+resfriamento1 decimal(5,2),
+resfriamento2 decimal(5,2),
+resfriamento3 decimal(5,2),
+fermentacao decimal(5,2),
+maturacao decimal(5,2),
+pasteurizacao decimal(5,2),
+servir decimal(5,2)
 );
 
-desc processos;
+desc processos3;
 
 
